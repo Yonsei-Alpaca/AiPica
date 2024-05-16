66b4de603e9d045d594151ac5eeac6e88bd853d9
jimin <ojimin00@gmail.com>
2024-05-17 01:32:12 +0900

    프로젝트 구조 변경

---
+++ src/main/java/com/Alpaca/AiPica/AiPicaApplication.java
@@ -0,0 +1,13 @@
+package com.alpaca.AiPica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Alpaca/AiPica/AiPicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpaca.AiPica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
